2d42473b2f9cf8e31084b9d764e23bb4339e8acb
Jacques Arnoux <arnoux@yahoo-inc.com>
2013-04-30 17:54:20 -0700

    added embryos of modules for template engine, perf and authentication modules

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
-var express = require('express'),
-    app = express();
+var express           = require('express'),
+    handlebarsAdapter = require('./lib/hb-adapter'),
+    perf              = require('./lib/perf'),
+    auth              = require('./lib/auth'),
+    app               = express();
+
+
+app.engine('.html', handlebarsAdapter);
+
+app.use(perf);
+app.use(express.cookieParser('secret'));
+app.use(express.session());
+app.use(function (req, res, next) {
+    req.session.user = 'jacquot';
+    next();
+});
+app.use(auth);
+
+app.use(function (req, res, next) {
+    app.render('index.hb.html', {
+        world: 'jacques'
+    },
+    function (err, html) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(html);
+        }
+    });
+});
+
+app.listen(3000);
+console.log('listening on port :', 3000);
