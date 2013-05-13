6f52c7e3357647fd84b54a197d7fd73fb516ff11
Jacques Arnoux <arnoux@yahoo-inc.com>
2013-05-13 11:46:09 -0700

    added the non-yet-working config middleware and logwriter middleware for developper error message

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 var express           = require('express'),
     handlebarsAdapter = require('./lib/hb-adapter'),
-    perf              = require('./lib/perf'),
     auth              = require('./lib/auth'),
+    config            = require('./lib/config'),
     app               = express();
 
 
-app.engine('.html', handlebarsAdapter);
+app.engine('.html', handlebarsAdapter());
 
-app.use(perf);
+
+app.use(app.router);
+app.use(express.static(__dirname + '/static'));
+
+app.use(config({
+    configFile: 'config.json',
+    app: app,
+    middlewares: [express.logger]
+}));
+// app.use(express.logger({
+//     format: '{date: ":date", msec: ":response-time", ' +
+//         'addr: ":remote-addr", usag: ":user-agent", refr: ":referrer", ' +
+//         'req: ":method :url", resp: ":status :res[content-length]"}'
+// }));
+
+app.use(express.query());
+app.use(express.bodyParser());
 app.use(express.cookieParser('secret'));
 app.use(express.session());
-app.use(function (req, res, next) {
-    req.session.user = 'jacquot';
-    next();
-});
-app.use(auth);
+
+app.use('/register', auth.register());
+app.use('/login', auth.login());
+app.use('/logout', auth.logout());
 
 app.use(function (req, res, next) {
+    var user = req.session.user;
     app.render('index.hb.html', {
-        world: 'jacques'
+        username: (user ? user.username : null)
     },
     function (err, html) {
         if (err) {
@@ -29,5 +45,7 @@ app.use(function (req, res, next) {
     });
 });
 
+app.use(express.errorHandler());
+
 app.listen(3000);
-console.log('listening on port :', 3000);
+console.log('app listening on port', 3000);
