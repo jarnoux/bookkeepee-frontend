18ff4d2c27bb3947645d0ce2d25da725e97a280b
Jacques Arnoux <arnoux@yahoo-inc.com>
2013-05-16 18:03:35 -0700

    many big changes in configuration module

--- server.js
+++ server.js
@@ -1,41 +1,30 @@
 var express           = require('express'),
+    async             = require('async'),
     handlebarsAdapter = require('./lib/hb-adapter'),
     auth              = require('./lib/auth'),
-    config            = require('./lib/config'),
+    Config            = require('./lib/config'),
+    config            = new Config('config.json'),
     app               = express();
-
 
 app.engine('.html', handlebarsAdapter());
 
-
 app.use(app.router);
-app.use(express.static(__dirname + '/static'));
-
-app.use(config({
-    configFile: 'config.json',
-    app: app,
-    middlewares: [express.logger]
-}));
-// app.use(express.logger({
-//     format: '{date: ":date", msec: ":response-time", ' +
-//         'addr: ":remote-addr", usag: ":user-agent", refr: ":referrer", ' +
-//         'req: ":method :url", resp: ":status :res[content-length]"}'
-// }));
-
-app.use(express.query());
-app.use(express.bodyParser());
-app.use(express.cookieParser('secret'));
-app.use(express.session());
+app.use(config.ure(express.static, 'express.static'));
+app.use(config.ure(express.logger, 'express.logger'));
+app.use(config.ure(express.query, 'express.query'));
+app.use(config.ure(express.bodyParser, 'express.bodyParser'));
+app.use(config.ure(express.cookieParser, 'express.cookieParser'));
+app.use(config.ure(express.session, 'express.session'));
 
 app.use('/register', auth.register());
 app.use('/login', auth.login());
 app.use('/logout', auth.logout());
-
 app.use(function (req, res, next) {
     var user = req.session.user;
     app.render('index.hb.html', {
         username: (user ? user.username : null)
     },
+
     function (err, html) {
         if (err) {
             res.send(err);
