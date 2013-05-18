983a807e425eb343155c7044579bae3e65bb04be
Jacques Arnoux <arnoux@yahoo-inc.com>
2013-05-17 22:59:23 -0700

    added registry facility to uniformize access and configuration of middlewares

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 var express           = require('express'),
     async             = require('async'),
+    path = require('path'),
     auth              = require('./middleware/auth'),
     handlebarsAdapter = require('./lib/hb-adapter'),
     Config            = require('./lib/config'),
     config            = new Config('config.json'),
-    app               = express();
+    app               = express(),
+    Registry = require('./lib/registry'),
+    registry = new Registry(config);
 
-app.use(config.ure(express.static, 'express.static'));
-app.use(config.ure(express.logger, 'express.logger'));
-app.use(config.ure(express.query, 'express.query'));
-app.use(config.ure(express.bodyParser, 'express.bodyParser'));
-app.use(config.ure(express.cookieParser, 'express.cookieParser'));
-app.use(config.ure(express.session, 'express.session'));
+registry.register(path.join(__dirname, 'lib'));
+registry.register(path.join(__dirname, 'middleware'));
+
+registry.register('express.static', express.static);
+registry.register('express.logger', express.logger);
+registry.register('express.query', express.query);
+registry.register('express.bodyParser', express.bodyParser);
+registry.register('express.cookieParser', express.cookieParser);
+registry.register('express.session', express.session);
+registry.register('express.errorHandler', express.errorHandler);
+
+app.use(registry['express.static']);
+app.use(registry['express.logger']);
+app.use(registry['express.query']);
+app.use(registry['express.bodyParser']);
+app.use(registry['express.cookieParser']);
+app.use(registry['express.session']);
 
 app.use(app.router);
+
 app.post('/register', config.ure(auth.register, 'auth.register'));
 app.post('/login', config.ure(auth.login, 'auth.login'));
 app.get('/logout', config.ure(auth.logout, 'auth.logout'));
 
-app.engine('.html', handlebarsAdapter());
+app.engine('.html', registry['hb-adapter']);
+
 app.use(function (req, res, next) {
     var user = req.session.user;
     app.render('index.hb.html', {
@@ -34,7 +50,8 @@ app.use(function (req, res, next) {
     });
 });
 
-app.use(express.errorHandler());
+
+app.use(registry['express.errorHandler']);
 
 app.listen(3000);
 console.log('app listening on port', 3000);
