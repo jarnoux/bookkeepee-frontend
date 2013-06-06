8690b0f4c27c92d51ba75471437f1e75c64f9454
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-06-05 17:48:43 -0700

    added strcture for backend user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,18 @@
 
-modules.exports = function (registry) {
-    return function (req, res, next) {
-
-    };
+modules.exports = {
+    login: function (options) {
+        return function login(req, res, next) {
+            // todo
+        };
+    },
+    logout: function (options) {
+        return function logout(req, res, next) {
+            // todo
+        };
+    },
+    register: function (options) {
+        return function register(req, res, next) {
+            // todo
+        };
+    }
 };
