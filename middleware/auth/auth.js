59a5d7e8cf7f72e031cff1c52bfd9b62c51a7d33
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-06-13 22:46:26 -0700

    changed the auth middleware so that it uses the bakend model instead of the user model that has no reason to exist on the frontend anymore

--- middleware/auth/auth.js
+++ middleware/auth/auth.js
@@ -1,55 +1,54 @@
-var authModule = function () {
-    'use strict';
-    var User;
-    return {
-        register: function () {
-            return function register(req, res, next) {
-                User = User || req.registry.get('models.user');
-                var newUser = new User({
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password
-                });
-                newUser.save(function (err) {
-                    if (err) {
-                        return next(err);
-                    }
-                    req.session.user = newUser;
-                    return res.redirect('/');
-                });
-            };
-        },
-        login: function () {
-            return function enticate(req, res, next) {
-                User = User || req.registry.get('models.user');
-                if (!req.session.user) {
-                    User.getAuthenticated(
-                        req.body.username,
-                        req.body.password,
-                        function (err, user, failReason) {
+var http        = require('http'),
+    querystring = require('querystring'),
+    authModule  = function auth() {
+        'use strict';
+        var User;
+        return {
+            register: function () {
+                return function register(req, res, next) {
+                    User = User || req.registry.get('models.user');
+                    var newUser = new User({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+                    newUser.save(function (err) {
+                        if (err) {
+                            return next(err);
+                        }
+                        req.session.user = newUser;
+                        return res.redirect('/');
+                    });
+                };
+            },
+            login: function () {
+                return function login(req, res, next) {
+                    var backend = req.registry.get('models.backend'),
+                        postBody;
+                    if (!req.session.user) {
+                        postBody = querystring.stringify(req.body);
+                        backend.call({
+                            path: '/login',
+                            method: 'post'
+                        }, postBody, function (err, result) {
                             if (err) {
                                 return next(err);
                             }
-                            if (!user) {
-                                return next(new Error('Invalid Credentials'));
-                            }
-                            // TODO: log the fail reason
-                            req.session.user = user;
+                            req.session.user = result;
                             return res.redirect('/');
-                        }
-                    );
-                } else {
-                    next();
-                }
-            };
-        },
-        logout: function () {
-            return function logout(req, res, next) {
-                delete req.session.user;
-                return res.redirect('/');
-            };
-        }
+                        }).on('error', next);
+                    } else {
+                        next();
+                    }
+                };
+            },
+            logout: function () {
+                return function logout(req, res, next) {
+                    delete req.session.user;
+                    return res.redirect('/');
+                };
+            }
+        };
     };
-};
 
 module.exports = authModule;
