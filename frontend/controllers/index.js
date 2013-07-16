10431122e9ec818aa20f2fd4e8f2010287f2d7e3
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-07-15 20:34:32 -0700

    added a home page with support for the backend

--- frontend/controllers/index.js
+++ frontend/controllers/index.js
@@ -1,8 +1,6 @@
 module.exports = function () {
     'use strict';
     return function (req, res, done) {
-        return done(null, {
-            user: req.session.user
-        });
+		done();
     };
 };
--- frontend/middleware/auth/auth.js
+++ frontend/middleware/auth/auth.js
@@ -1,41 +1,36 @@
-var http        = require('http'),
-    querystring = require('querystring'),
-    authModule  = function auth() {
+var retrieveUser = function (path, req, res, next) {
         'use strict';
-        var User,
-            retrieveUser = function (path, req, res, next) {
-                var backend = req.registry.get('models.backend'),
-                    postBody;
-                if (!req.session.user) {
-                    postBody = querystring.stringify(req.body);
-                    backend.request({
-                        path: path,
-                        method: 'post'
-                    }, postBody, function (err, result) {
-                        if (err) {
-                            return next(err);
-                        }
-                        req.session.user = result;
-                        return res.redirect('/');
-                    }).on('error', next);
-                } else {
-                    next();
-                }
-            };
-        return {
-            register: function configureRegister() {
-                return retrieveUser.bind(null, '/register');
-            },
-            login: function configureLogin() {
-                return retrieveUser.bind(null, '/login');
-            },
-            logout: function configureLogout() {
-                return function logout(req, res, next) {
-                    delete req.session.user;
-                    return res.redirect('/');
-                };
-            }
-        };
-    };
+        var backend = req.registry.get('models.backend');
 
-module.exports = authModule;
+        if (!req.session.user) {
+            backend.request({
+                path: path,
+                method: 'post'
+            }, req.body, function (err, result) {
+                if (err) {
+                    return next(err);
+                }
+                req.session.user = result;
+                return res.redirect('/home');
+            }).on('error', next);
+        } else {
+            next();
+        }
+    };
+module.exports = {
+    register: function () {
+        'use strict';
+        return retrieveUser.bind(null, '/register');
+    },
+    login: function () {
+        'use strict';
+        return retrieveUser.bind(null, '/login');
+    },
+    logout: function () {
+        'use strict';
+        return function logout(req, res, next) {
+            delete req.session.user;
+            return res.redirect('/');
+        };
+    }
+};
