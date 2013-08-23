5e9d521e81c6ce78cf8045b25672579dee0fdf3f
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-08-22 18:39:46 -0700

    added photo model and middleware and tweaked the routes

--- static/unit.js
+++ static/unit.js
@@ -6,26 +6,30 @@ var form = $('form'),
     inputs = $('input, textarea, select', form),
     fileInput = $('input[type=file]'),
     toast = $('.toast');
-// inputs.blur(function (e) {
-//  var target = $(e.target),
-//      form = target.parents('form');
+inputs.change(function (e) {
+ var target = $(e.target),
+     form = target.parents('form');
 
-//  target.switchClass('error', 'highlight', 400);
+ target.switchClass('error', 'highlight', 400);
 
-//  $.post(form.attr('action'), form.serialize(), function (data) {
-//      target.removeClass('highlight', 400);
-//  }).fail(function (e) {
-//      target.removeClass('highlight');
-//      target.toggleClass('error', false, 400).toggleClass('error', 400);
-//  });
-// });
+ $.post(form.attr('action'), form.serialize(), function (data) {
+     target.removeClass('highlight', 400);
+ }).fail(function (e) {
+     target.removeClass('highlight');
+     target.toggleClass('error', false, 400).toggleClass('error', 400);
+ });
+});
 
-// fileInput.change(function (e) {
-//  var target = $(e.target),
-//      form = target.parents('form');
+fileInput.change(function (e) {
+	var target = $(e.target),
+		form = target.parents('form'),
+		submit = $('input[type=submit]', form);
+	submit.removeAttr('disabled')
+		.removeAttr('title')
+		.click(function () {
+			submit.attr('value', 'Uploading...');
+			return true;
+		});
 
-//      $.post(form.attr('action'), form.serialize(), function (data) {
+});
 
-//      });
-//  console.log('[unit.js:20] file upload');
-// });
