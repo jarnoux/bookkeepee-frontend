5ab4e8229342c7c7804b5850f5c5d56faa57a72a
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-09-09 20:11:22 -0700

    added date picker and slot number to the view + lots of css/client js

--- static/unit.js
+++ static/unit.js
@@ -6,19 +6,22 @@ var ajaxForm = $('form.ajaxInputs'),
     ajaxInputs = $('input, textarea, select', ajaxForm),
     fileForm = $('form.fileInputs'),
     fileInput = $('input[type=file]', fileForm),
-    toast = $('.toast');
+    toast = $('.toast'),
+    visitForm = $('#visitForm'),
+    newVisitActionIcon = $('#newVisit');
+
 ajaxInputs.change(function (e) {
- var target = $(e.target),
-     form = target.parents('form');
+	var target = $(e.target),
+		form = target.parents('form');
 
- target.switchClass('error', 'highlight', 400);
+	target.switchClass('error', 'highlight', 400);
 
- $.post(form.attr('action'), form.serialize(), function (data) {
-     target.removeClass('highlight', 400);
- }).fail(function (e) {
-     target.removeClass('highlight');
-     target.toggleClass('error', false, 400).toggleClass('error', 400);
- });
+	$.post(form.attr('action'), form.serialize(), function (data) {
+		target.removeClass('highlight', 400);
+	}).fail(function (e) {
+		target.removeClass('highlight');
+		target.toggleClass('error', false, 400).toggleClass('error', 400);
+	});
 });
 
 fileInput.change(function (e) {
@@ -31,6 +34,15 @@ fileInput.change(function (e) {
 			submit.attr('value', 'Uploading...').addClass('pure-button-disabled');
 			return true;
 		});
-
 });
 
+$('#visitDate').datepicker({
+	prevText: '<i class="icon-chevron-left"></i>',
+	nextText: '<i class="icon-chevron-right"></i>',
+	minDate: new Date(),
+	onSelect: function (dateText, datePicker) {
+		newVisitActionIcon.show();
+	}
+}).click(function () {
+	$('.ui-datepicker-next, .ui-datepicker-prev').attr('title', '');
+});
